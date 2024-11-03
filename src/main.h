bc0dfdfc5afb3298a81e49d6ae7d00df9e75669c
Andrew McDaniel <andrewmcdan@gmail.com>
2024-11-02 23:17:59 -0400

    nothing

--- src/main.h
+++ src/main.h
@@ -29,19 +29,19 @@
 #endif
 #endif
 
-#include <QtBluetooth/QBluetoothLocalDevice>
-#include <QtBluetooth/QLowEnergyAdvertisingData>
-#include <QtBluetooth/QLowEnergyCharacteristic>
-#include <QtBluetooth/QLowEnergyDescriptor>
-#include <QtBluetooth/QLowEnergyService>
-#include <QtBluetooth/QLowEnergyController>
-#include <QtBluetooth/QBluetoothUuid>
-#include <QtBluetooth/QBluetoothAddress>
-#include <QtBluetooth/QBluetoothDeviceInfo>
-#include <QtBluetooth/QBluetoothDeviceDiscoveryAgent>
-#include <QtBluetooth/QBluetoothLocalDevice>
-#include <QtBluetooth/QBluetoothServer>
-#include <QtBluetooth/QBluetoothSocket>
+// #include <QtBluetooth/QBluetoothLocalDevice>
+// #include <QtBluetooth/QLowEnergyAdvertisingData>
+// #include <QtBluetooth/QLowEnergyCharacteristic>
+// #include <QtBluetooth/QLowEnergyDescriptor>
+// #include <QtBluetooth/QLowEnergyService>
+// #include <QtBluetooth/QLowEnergyController>
+// #include <QtBluetooth/QBluetoothUuid>
+// #include <QtBluetooth/QBluetoothAddress>
+// #include <QtBluetooth/QBluetoothDeviceInfo>
+// #include <QtBluetooth/QBluetoothDeviceDiscoveryAgent>
+// #include <QtBluetooth/QBluetoothLocalDevice>
+// #include <QtBluetooth/QBluetoothServer>
+// #include <QtBluetooth/QBluetoothSocket>
 
 #ifndef PROJECT_VERSION_STRING
 #define PROJECT_VERSION_STRING "0.0.0"
